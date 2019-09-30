dc0e11776c2b7ec0a2434ff0a94b031f0626df9b
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-30 00:42:02 -0400

    6.6 done

--- Convertit/ViewController.swift
+++ Convertit/ViewController.swift
@@ -17,12 +17,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var formulaPicker: UIPickerView!
     @IBOutlet weak var decimalSegment: UISegmentedControl!
     
+    @IBOutlet weak var signSegment: UISegmentedControl!
+    
     var formulaArray = ["miles to kilometers" ,
                         "kilometers to miles",
                         "feet to meters",
                         "meters to feet",
                         "yards to meters",
-                        "meters to yards"]
+                        "meters to yards",
+                        "inches to cm",
+                        "cm to inches",
+                        "fahrenheit to celsius",
+                        "celsius to fahrenheit",
+                        "quarts to liters",
+                        "liters to quarts"]
     
     var fromUnits = ""
     var toUnits = ""
@@ -33,12 +41,15 @@ class ViewController: UIViewController {
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
         conversionString = formulaArray[formulaPicker.selectedRow(inComponent: 0)]
+        userInput.becomeFirstResponder()
+        signSegment.isHidden = true
     }
 
-    
     func calculateConversion() {
         guard let inputValue = Double(userInput.text!) else {
-            print("show alert!")
+            if userInput.text != "" {
+                showAlert(title: "Cannot Convert Value", message: "\"\(userInput.text!) is not a volaid number!")
+            }
             return
         }
         
@@ -56,8 +67,20 @@ class ViewController: UIViewController {
             outputValue = inputValue / 1.0936
         case "meters to yards":
             outputValue = inputValue * 1.0936
+        case "inches to cm":
+            outputValue = inputValue / 0.3937
+        case "cm to inches":
+            outputValue = inputValue * 0.3937
+        case "fahrenheit to celsius":
+            outputValue = (inputValue - 32) * (5/9)
+        case "celsius to fahrenheit":
+            outputValue = (inputValue * 9/5) + 32
+        case "quarts to liters":
+            outputValue = inputValue / 1.05669
+        case "liters to quarts":
+            outputValue = inputValue * 1.05669
         default:
-            print("Show Alert!")
+            showAlert(title: "Unexpected Error", message: "\(conversionString) cannot be identified! :(")
         }
         let formatString = (decimalSegment.selectedSegmentIndex < decimalSegment.numberOfSegments ? "%.\(decimalSegment.selectedSegmentIndex+1)f": "%f")
         let outputString = String(format: formatString, outputValue)
@@ -65,9 +88,39 @@ class ViewController: UIViewController {
  
     }
     
-    @IBAction func decimalSelected(_ sender: Any) {
+    func showAlert(title:String, message:String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default , handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
     }
-    @IBAction func convertButtonPressed(_ sender: Any) {
+    @IBAction func userInputChanged(_ sender: UITextField) {
+        resultsLabel.text = ""
+        if userInput.text?.first == "-" {
+            signSegment.selectedSegmentIndex = 1
+        } else{
+            signSegment.selectedSegmentIndex = 0
+        }
+    }
+    
+    @IBAction func decimalSelected(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signSegmentSelected(_ sender: UISegmentedControl) {
+        if signSegment.selectedSegmentIndex == 0 {
+            userInput.text = userInput.text?.replacingOccurrences(of: "-", with: "")
+        } else {
+            userInput.text = "-" + userInput.text!
+        }
+        if userInput.text != "-"{
+            calculateConversion()
+        }
+
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        calculateConversion()
     }
     
 }
@@ -84,6 +137,14 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         conversionString = formulaArray[row]
+        
+        if conversionString.lowercased().contains("celsius".lowercased()) {
+            signSegment.isHidden = false
+        }else {
+            signSegment.isHidden = true
+            userInput.text = userInput.text?.replacingOccurrences(of: "-", with: "")
+            signSegment.selectedSegmentIndex = 0
+        }
         let unitsArray = formulaArray[row].components(separatedBy: " to ")
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
